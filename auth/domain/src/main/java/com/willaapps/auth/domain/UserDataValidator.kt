1830b06726939f28b99345fdfe9116408eace033
Wilie12 <albertw536@gmail.com>
2024-07-25 21:08:38 +0200

    Validating user data

--- auth/domain/src/main/java/com/willaapps/auth/domain/UserDataValidator.kt
+++ auth/domain/src/main/java/com/willaapps/auth/domain/UserDataValidator.kt
@@ -1,6 +1,30 @@
 package com.willaapps.auth.domain
 
-class UserDataValidator {
+class UserDataValidator(
+    private val patternValidator: PatternValidator
+) {
+
+    fun isValidUsername(username: String): Boolean {
+        return (username.isNotBlank() && username.isNotEmpty())
+    }
+
+    fun isValidEmail(email: String): Boolean {
+        return patternValidator.matches(email.trim())
+    }
+
+    fun validatePassword(password: String): PasswordValidationState {
+        val hasMinLength = password.length >= MIN_PASSWORD_LENGTH
+        val hasNumber = password.any { it.isDigit() }
+        val hasLowerCaseCharacter = password.any { it.isLowerCase() }
+        val hasUpperCaseCharacter = password.any { it.isUpperCase() }
+
+        return PasswordValidationState(
+            hasMinLength = hasMinLength,
+            hasNumber = hasNumber,
+            hasLowerCaseCharacter = hasLowerCaseCharacter,
+            hasUpperCaseCharacter = hasUpperCaseCharacter
+        )
+    }
 
     companion object {
         const val MIN_PASSWORD_LENGTH = 9
